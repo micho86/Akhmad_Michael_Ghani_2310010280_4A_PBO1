7172c3afcb5c471306441c4121c0a6cb9cb3d47c
micho86 <64905516+micho86@users.noreply.github.com>
2025-07-29 08:25:28 +0800

    Create BukuFiksi.java

---
+++ BukuFiksi.java
@@ -0,0 +1,15 @@
+package buku;
+
+public class BukuFiksi extends Buku {
+    private String genre;
+    
+    public BukuFiksi(String judul, String pengarang, int tahunTerbit) {
+        super(judul, pengarang, tahunTerbit);
+        this.genre = "Fiksi Umum";
+    }
+    
+    @Override
+    public String getKategori() {
+        return "Buku Fiksi - Genre: " + genre;
+    }
+}
