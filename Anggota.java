54a94269b0d9cdf776267730f42447ba1390aadd
micho86 <64905516+micho86@users.noreply.github.com>
2025-07-29 08:32:25 +0800

    Anggota.java

--- Anggota.java
+++ Anggota.java
@@ -7,6 +7,7 @@ public class Anggota {
     private String nama;
     private String idAnggota;
     private ArrayList<Buku> bukuDipinjam;
+    private static final int MAX_PINJAM = 3;
     
     public Anggota(String nama, String idAnggota) {
         this.nama = nama;
@@ -15,12 +16,36 @@ public class Anggota {
     }
     
     public void pinjamBuku(Buku buku) {
-        if(!buku.isDipinjam()) {
+        if(!buku.isDipinjam() && bukuDipinjam.size() < MAX_PINJAM) {
             buku.setDipinjam(true);
             bukuDipinjam.add(buku);
-            System.out.println(nama + " meminjam " + buku.getJudul());
+            System.out.println(nama + " berhasil meminjam " + buku.getJudul());
+        } else if (bukuDipinjam.size() >= MAX_PINJAM) {
+            System.out.println("Maaf, " + nama + " sudah mencapai batas peminjaman");
+        } else {
+            System.out.println("Buku sedang dipinjam oleh anggota lain");
         }
     }
     
-    // Other methods...
+    public void kembalikanBuku(Buku buku) {
+        if(bukuDipinjam.remove(buku)) {
+            buku.setDipinjam(false);
+            System.out.println(nama + " berhasil mengembalikan " + buku.getJudul());
+        } else {
+            System.out.println("Buku tidak ditemukan dalam daftar pinjaman");
+        }
+    }
+    
+    public String getNama() {
+        return nama;
+    }
+    
+    public String getIdAnggota() {
+        return idAnggota;
+    }
+    
+    public ArrayList<Buku> getBukuDipinjam() {
+        return bukuDipinjam;
+    }
+}
 }
